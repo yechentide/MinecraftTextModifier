53e5f244cb1496db8023dca24dad31785dfe8b4b
yechentide <yechentide@gmail.com>
2020-03-29 11:42:32 +0900

    first commit

---
+++ cmake-build-debug/CMakeFiles/MinecraftTextModifier.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MinecraftTextModifier.dir/main.cpp.o"
+  "MinecraftTextModifier"
+  "MinecraftTextModifier.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MinecraftTextModifier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
